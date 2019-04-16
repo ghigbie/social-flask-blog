024a732e3a1cb39b03124549333aec236536a35a
ghigbie <gehigbie@gmail.com>
2019-04-15 21:18:27 -0600

    Import database

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,12 +1,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 ###### DATABASE SETUP ########
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
