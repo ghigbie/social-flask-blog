ff912ee1b40124fbeaac4f83dfeec8d0056cba26
ghigbie <gehigbie@gmail.com>
2019-04-16 20:07:26 -0600

    Add picture file

--- puppycompanyblog/users/forms.py
+++ puppycompanyblog/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, 
 from wtforms import ValidationError
 from flask_wtf.file import FileField, FileAllowed
 
@@ -17,12 +17,27 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username: ', validators=[DataRequired()])
     password = PasswordField('Password: ', validators=[DataREquired(), EqualTo('pass_corfirm'), message="Passwords must match"])
     pass_confirm = PasswordField('Confirm Password: ', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 
-def check_email(self, field):
-    if User.query.filter_by(email=field.data).first():
-        raise ValidationError('Your email is already in use. Please choose another.')
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email is already in use. Please choose another.')
 
-def check_username(self, field):
-    if User.query.filter_by(email=field.data).first():
-        raise ValidationError('Your username is already in use. Please choose another.')
+    def check_username(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your username is already in use. Please choose another.')
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    username = StringField('Username: ', validators=[DataRequired()])
+    picture = FieldField('Update profile picture: ', validators=[FileAllowed(['png', 'jpg'])])
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email is already in use. Please choose another.')
+
+    def check_username(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your username is already in use. Please choose another.')
