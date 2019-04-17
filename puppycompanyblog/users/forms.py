add86a5bb1536220de3e8079879accd55a7f431b
ghigbie <gehigbie@gmail.com>
2019-04-16 19:28:52 -0600

    Add loginform class

---
+++ puppycompanyblog/users/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_user
+from puppycompanyblog import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password: ', validators=[DataRequired()])
+    submit = SubmitField('Login')
