2a92cbda559e785cea15b7ea7cd27a0880b667a2
ghigbie <gehigbie@gmail.com>
2019-04-16 19:48:45 -0600

    Add methods for registration form

--- puppycompanyblog/users/forms.py
+++ puppycompanyblog/users/forms.py
@@ -11,3 +11,18 @@ class LoginForm(FlaskForm):
     email = StringField('Email: ', validators=[DataRequired(), Email()])
     password = PasswordField('Password: ', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    username = StringField('Username: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataREquired(), EqualTo('pass_corfirm'), message="Passwords must match"])
+    pass_confirm = PasswordField('Confirm Password: ', validators=[DataRequired()])
+
+
+def check_email(self, field):
+    if User.query.filter_by(email=field.data).first():
+        raise ValidationError('Your email is already in use. Please choose another.')
+
+def check_username(self, field):
+    if User.query.filter_by(email=field.data).first():
+        raise ValidationError('Your username is already in use. Please choose another.')
