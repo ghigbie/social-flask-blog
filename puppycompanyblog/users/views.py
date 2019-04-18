f7af6159881f2fbf8b49d791d46a02d5b48a35a6
ghigbie <gehigbie@gmail.com>
2019-04-17 20:17:50 -0600

    Add register route and logut route

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -4,3 +4,24 @@ from puppycompanyblog import db
 from puppycompanyblog.models import User, BlogPost
 from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUsersForm
 from puppycompanyblog.users.picture_handler import add_profile_pic
+
+users = Blueprint('users',__name__)
+
+@users.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('users.login'))
+
+
+
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("core.index"))
+
