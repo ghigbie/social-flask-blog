a5b5bfc0452453e32bc3d03e5ccd0bca38591cac
ghigbie <gehigbie@gmail.com>
2019-04-20 08:52:53 -0600

    Fix typo

--- puppycompanyblog/models.py
+++ puppycompanyblog/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
-    posts = db.relatioship('BlogPost', backref='author', lazy=True)
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
