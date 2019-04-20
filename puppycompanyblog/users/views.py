514f495a6cd5be794e21c0fc5ad05d79c8a881fe
ghigbie <gehigbie@gmail.com>
2019-04-20 09:09:32 -0600

    Fix typo

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from puppycompanyblog import db
 from puppycompanyblog.models import User, BlogPost
-from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUsersForm
+from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from puppycompanyblog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
