ef6716f0a11ef5a351b6dd1b23b2b048b952e5a7
ghigbie <gehigbie@gmail.com>
2019-04-20 08:48:39 -0600

    Edit parameters

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -31,7 +31,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data,
-                    username=form.username.data
+                    username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
