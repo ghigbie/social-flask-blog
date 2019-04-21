12369abd21b6915db77fac515f82e9dafbe30a4f
ghigbie <gehigbie@gmail.com>
2019-04-21 09:07:24 -0600

    Create create route for blog posts

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -6,3 +6,17 @@ from puppycompanyblog.blog_posts.forms import BlogPostForm
 
 blog_posts = Blueprint('blog_posts', __name__)
 
+@blog_posts.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_post()
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        blog_post = BlogPost(title=form.title.data,
+                             text=form.text.data,
+                             user_id=current_user.id)
+        db.session.add(blog_post)
+        db.sesson.commit()
+        flash('Blog Post Created')
+        return redirect(url_for('core.index'))
+    return render_template('create_post.html', form=form)
+    
