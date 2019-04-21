f8378e0e1d7e98849ac05fb2509394366669d979
ghigbie <gehigbie@gmail.com>
2019-04-21 10:02:24 -0600

    Edit typos

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -8,7 +8,7 @@ blog_posts = Blueprint('blog_posts', __name__)
 
 @blog_posts.route('/create', methods=['GET', 'POST'])
 @login_required
-def create_post()
+def create_post():
     form = BlogPostForm()
     if form.validate_on_submit():
         blog_post = BlogPost(title=form.title.data,
@@ -41,3 +41,22 @@ def update(blog_post_id):
         db.session.commit()
         flash('Blog Post Updated')
         return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
+
+    elif request.method = 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html', title="Updating", form=form)
+
+
+@blog_post.route('/<int:blog_post_id/delete', methods=['GET', 'POST'])
+@login_required
+def delete_post(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post_id.author != current_user:
+        abort(403)
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Blog Post Deleted')
+    return redirect(url_for('core.index'))
+    
