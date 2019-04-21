96cc6e063b5408761fee22a91a15791c608614af
ghigbie <gehigbie@gmail.com>
2019-04-21 10:06:55 -0600

    Debug url problems

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -20,15 +20,15 @@ def create_post():
         return redirect(url_for('core.index'))
     return render_template('create_post.html', form=form)
 
-@blog_post.route('/<int:blog_post_id>')
+@blog_posts.route('/<int:blog_post_id>')
 def blog_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
     return render_template('blog_post.html',
-                            title=blog_post.title
-                            date=blog_post.date
+                            title=blog_post.title,
+                            date=blog_post.date,
                             post=blog_post.post) 
 
-@blog_post.route('/<int:blog_post_id/update', methods=['GET', 'POST'])
+@blog_posts.route('/<int:blog_post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -42,14 +42,14 @@ def update(blog_post_id):
         flash('Blog Post Updated')
         return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
 
-    elif request.method = 'GET':
+    elif request.method == 'GET':
         form.title.data = blog_post.title
         form.text.data = blog_post.text
 
     return render_template('create_post.html', title="Updating", form=form)
 
 
-@blog_post.route('/<int:blog_post_id/delete', methods=['GET', 'POST'])
+@blog_posts.route('/<int:blog_post_id>/delete', methods=['GET', 'POST'])
 @login_required
 def delete_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
