275c62480389146ac9a7bb95b2147110b2302ab6
ghigbie <gehigbie@gmail.com>
2019-04-22 21:33:21 -0600

    Fix link problems

--- puppycompanyblog/core/views.py
+++ puppycompanyblog/core/views.py
@@ -7,8 +7,8 @@ core = Blueprint('core', __name__)
 @core.route('/index')
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_post = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
-    return render_template('index.html', blog_post=blog_post)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
