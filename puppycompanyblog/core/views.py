e8bcd3d6c68bdf5cb537ebff266747d33026b1f1
ghigbie <gehigbie@gmail.com>
2019-04-22 21:14:47 -0600

    Fix bugs

--- puppycompanyblog/core/views.py
+++ puppycompanyblog/core/views.py
@@ -7,7 +7,7 @@ core = Blueprint('core', __name__)
 @core.route('/index')
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_post = BlogPost.query.order_by(BlogPost.date.desc()).pageinate(page=page, per_page=5)
+    blog_post = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
     return render_template('index.html', blog_post=blog_post)
 
 @core.route('/info')
